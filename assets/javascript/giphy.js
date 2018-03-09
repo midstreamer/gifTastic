76f70b29c3866edd1374ce43594bcddfa784fb30
midstreamer <chadalessi@yahoo.com>
2018-03-09 08:37:40 -0800

    added style to the DOM

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,9 +1,6 @@
-//Before you can make any part of our site work, you need to create an array of strings, each one related to a topic that interests you.Save it to a variable called`topics`.
-   //We chose animals for our theme, but you can make a list to your own liking.-->
+var animal = ["cat", "goat", "bird", "pig", "skunk", "duck", "cow", "frog", "fish", "bear", "moose", "turtle"];
 
-   var animal = ["cat", "dog", "bird", "pig", "skunk", "duck", "cow", "frog", "fish", "bear", "moose", "turtle"];
-
-    function renderButtons() {
+    function renderButtons() { //this function will loop through the array and render buttons for each animal
 
       $("#animal-view").empty();
 
@@ -12,106 +9,131 @@
         var a = $("<button>");
         a.addClass("animal");
         a.attr("data-name", animal[i]);
-        // Providing the button's text with a value of the movie at index i
-        a.text(animal[i]);
+        a.text(animal[i]);  // This provides the button's text with a value of the animal at index i
 
-        // chain jQuery Methods
-        // var a = $("<button>").addClass("movie").attr("data-name", movies[i]).text(movies[i]);
-        // Adding the button to the HTML
         $("#animal-view").append(a);
       }
     }
-    // This function handles events where one button is clicked
-    $("#add-animal").on("click", function (event) {
-      // event.preventDefault() prevents the form from trying to submit itself.
-      // We're using a form so that the user can hit enter instead of clicking the button if they want
-      event.preventDefault();
 
-      // This line will grab the text from the input box
-      var animalInput = $("#animal-input").val().trim();
-      // The animal from the textbox is then added to our array
-      animal.push(animal);
+    renderButtons();  // Calling the renderButtons function at least once to display the initial list of animals
 
-      // calling renderButtons which handles the processing of our animal array
-      renderButtons();
-    });
-
-    // Calling the renderButtons function at least once to display the initial list of movies
-    renderButtons();
-
-    $("button").on("click", function () {
+    $("button").on("click", function () { //this on click event will query the Giphy API for the animal in the array 
 
       var animalName = $(this).attr("data-name");
 
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animalName + "&api_key=dc6zaTOxFJmzC&limit=10";
+        animalName + "&api_key=sHH4T81tmO7kT9Nb2jYBX8S5Fz7jZkJo&limit=10";
 
       $.ajax({
         url: queryURL,
         method: "GET"
       })
 
-      .then(function(response) {
+        .then(function (response) {
           console.log(queryURL);
 
           console.log(response);
-          // storing the data from the AJAX request in the results variable
-          var results = response.data;
 
-          // Looping through each result item
-          for (var i = 0; i < results.length; i++) {
+          var results = response.data; // This stores the data from the AJAX request in the results variable
 
-            // Creating and storing a div tag
-            var animalDiv = $("<div>");
+          for (var i = 0; i < results.length; i++) { // This loops through each result item
 
-            // Creating a paragraph tag with the result item's rating
-            var p = $("<p>").text("Rating: " + results[i].rating);
+            var animalDiv = $("<div>"); // Creates and stores a div tag
 
-            // Creating and storing an image tag
-            var animalImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            animalImage.attr("src", results[i].images.fixed_height.url);
+            var p = $("<p>").text("Rating: " + results[i].rating); // This creates a paragraph tag with the result item's rating
 
-            // Appending the paragraph and image tag to the animalDiv
-            animalDiv.append(p);
+            var animalImage = $("<img>");// Creating and storing an image tag
+
+            animalImage.attr("src", results[i].images.fixed_height_still.url); // This sets the src attribute of the image to a property pulled off the result item
+            animalImage.attr("class", "gif"); // Added this class do that we can use it later to call the gif and animate it or stop it
+            animalImage.attr("data-state", "still"); //assigns a data state of still to the gif
+            animalImage.attr("data-still", results[i].images.fixed_height_still.url); //assigns the url for the stil image to the data-still attribute
+            animalImage.attr("data-dynamic", results[i].images.fixed_height.url); //assigns the url for the dynamic image to the data-dynamic attribute
+
+
+            animalDiv.append(p); // Appending the paragraph and image tag to the animalDiv
             animalDiv.append(animalImage);
             animalDiv.append(p, animalImage); //this works the same as the two above lines.
 
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-            $("#gifs-appear-here").prepend(animalDiv);
-            
+            $("#gifs-appear-here").prepend(animalDiv); // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+
           }
         });
     });
-    $(".gif").on("click", function() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+
+    $("#add-animal").on("click", function (event) { // This function handles events where one of the buttons is clicked
+
+      event.preventDefault(); // event.preventDefault() prevents the form from trying to submit itself.
+
+      var animalInput = $("#animal-input").val().trim();  // This line will grab the text from the input box
+
+      animal.push(animalInput); // The animal from the textbox is then added to our array
+
+      renderButtons();  // calling renderButtons which handles the processing of our animal array with the new animal added
+
+      $("button").on("click", function () { //this on click event will query the Giphy API for the animal in the array 
+
+        var animalName = $(this).attr("data-name");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          animalName + "&api_key=sHH4T81tmO7kT9Nb2jYBX8S5Fz7jZkJo&limit=10";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+
+          .then(function (response) {
+            console.log(queryURL);
+
+            console.log(response);
+
+            var results = response.data; // This stores the data from the AJAX request in the results variable
+
+            for (var i = 0; i < results.length; i++) { // This loops through each result item
+
+              var animalDiv = $("<div>"); // Creates and stores a div tag
+
+              var p = $("<p>").text("Rating: " + results[i].rating); // This creates a paragraph tag with the result item's rating
+
+              var animalImage = $("<img>");// Creating and storing an image tag
+
+              animalImage.attr("src", results[i].images.fixed_height_still.url); // This sets the src attribute of the image to a property pulled off the result item
+              animalImage.attr("class", "gif"); // Added this class do that we can use it later to call the gif and animate it or stop it
+              animalImage.attr("data-state", "still"); //assigns a data state of still to the gif
+              animalImage.attr("data-still", results[i].images.fixed_height_still.url); //assigns the url for the stil image to the data-still attribute
+              animalImage.attr("data-dynamic", results[i].images.fixed_height.url); //assigns the url for the dynamic image to the data-dynamic attribute
+
+
+              animalDiv.append(p); // Appending the paragraph and image tag to the animalDiv
+              animalDiv.append(animalImage);
+              animalDiv.append(p, animalImage); //this works the same as the two above lines.
+
+              $("#gifs-appear-here").prepend(animalDiv); // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+
+            }
+          });
+      });
+
+    });
+
+    $(document).on("click", ".gif", function () { // This function will change the data state of the gif if it is clicked on.
+      console.log("im here")
+
+      var state = $(this).attr("data-state"); // this sets the value of the attribute data-state to the variable state 
+
       // Then, set the image's data-state to animate
       // Else set src to the data-still value
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
+
+      var still = $(this).attr("data-still");
+      var dynamic = $(this).attr("data-dynamic");
+
+
+      if (state === "still") { //this checks to see if the data state of the gif is still 
+        $(this).attr("src", dynamic); // Then, if it is still this will set the image's data-state to animate
         $(this).attr("data-state", "animate");
       } else {
-        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("src", still); // Else sets the data state to animate
         $(this).attr("data-state", "still");
       }
     });
- 
-
-//Your app should take the topics in this array and create buttons in your HTML.
-   //Try using a loop that appends a button for each string in the array.
-
-//When the user clicks on a button, the page should grab 10 static, non - animated gif images from the GIPHY API and place them on the page.
-
-//When the user clicks one of the still GIPHY images, the gif should animate.If the user clicks the gif again, it should stop playing.
-
-//Under every gif, display its rating(PG, G, so on).
-   //This data is provided by the GIPHY API.
-   //Only once you get images displaying with button presses should you move on to the next step.
-
-//Add a form to your page takes the value from a user input box and adds it into your`topics` array.Then make a function call that takes each topic in the array remakes the buttons on the page.
-
-//Deploy your assignment to Github Pages.
-
-//** Rejoice ** !You just made something really cool.-->
